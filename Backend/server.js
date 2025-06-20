b9be2a9887d7db2d094b522c2bb75894ae4cdb3f
Kiranb23 <kbudupula333@gmail.com>
2025-06-20 13:47:51 +0530

    Fetch

--- Backend/server.js
+++ Backend/server.js
@@ -25,5 +25,5 @@ app.get('/news', async (req, res) => {
     }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
