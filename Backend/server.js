023b5a38adcb58e839e8230bf48c7b77e5eccdb5
Harry <kbudupula333@gmail.com>
2025-06-20 10:55:15 +0000

    Hide API Key

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const app = express();
 // Use cors to avoid cors error across the browsers
 app.use(cors());
 
-const API_KEY = 'ffa1188e06627f4744c47e4874396e81';
+const API_KEY = 'ffa1188e06627f4744c47e4874396e81'; // Remove Here
 
 // Fetch news with optional pagination and search
 app.get('/news', async (req, res) => {
